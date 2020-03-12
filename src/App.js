2d41b01bb5806b98d35da86625ae819a6c03a7dc
vivilimaDD <43736742+vivilimaDD@users.noreply.github.com>
2020-03-11 22:45:12 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import Navegador from './Navegador';
+import Galeria from './Galeria';
+
+function MyFace(){
+    return (<div>
+      <Navegador></Navegador>
+      <Galeria></Galeria>
+      </div>
+      );
+}
+
+export default MyFace;
---
+++ src/Navegador.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css'
+
+function Navegador(){
+    return (<div> 
+                <nav className='header'>
+                <div className='menu'>
+                    <figure class="logo">
+                        <div>MyFace</div>
+                    </figure>
+                    <div className="usuario">
+                        <figure class="nome">Vivi Lima</figure>
+                    </div>
+                
+                <ul>
+                    <li>
+                        <a href="link">Linha do tempo</a>
+                    </li>
+                    <li>
+                        <a href="link">Perfil</a>
+                    </li>
+                    
+                </ul>
+                </div>
+            </nav>
+            </div>
+            
+            );
+}
+
+export default Navegador;
