3672b4c875615b0db0944d5aebdd01775fbb706d
vivilimah <31325881+vivilimah@users.noreply.github.com>
2020-03-13 13:30:46 -0300

    Update Navegador.js

--- src/Navegador.js
+++ src/Navegador.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css'
 
-function Navegador(){
-    return (<div> 
-                <nav className='header'>
-                <div className='menu'>
-                    <figure class="logo">
-                        <div>MyFace</div>
-                    </figure>
-                    <div className="usuario">
-                        <figure class="nome">Vivi Lima</figure>
-                    </div>
-                
+function Navegador() {
+    return (<div>
+        <nav className='header'>
+            <div className='menu'>
+                <figure class="logo">
+                    <div>MyFace</div>
+                </figure>
+                <div className="usuario">
+                    <figure class="nome">Vivi Lima</figure>
+                </div>
+
                 <ul>
                     <li>
                         <a href="link">Linha do tempo</a>
@@ -19,13 +19,13 @@ function Navegador(){
                     <li>
                         <a href="link">Perfil</a>
                     </li>
-                    
+
                 </ul>
-                </div>
-            </nav>
             </div>
-            
-            );
+        </nav>
+    </div>
+
+    );
 }
 
 export default Navegador;
